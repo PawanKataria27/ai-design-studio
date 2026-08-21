0b2cf8c1bc2f8c95c48e4cb791f8e1c8e7d7c569
PawanKataria27 <mahadevgraphicsjind7@gmail.com>
2026-08-21 01:56:42 -0700

    Use Supabase publishable key API

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
     url: _supabaseUrl,
-    anonKey: _supabasePublishableKey,
+    publishableKey: _supabasePublishableKey,
   );
   runApp(const MGDesignStudioApp());
 }
